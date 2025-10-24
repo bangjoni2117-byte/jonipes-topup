b8c872a43978e65e9baacf852239cb03c493a8f7
jonipes <bangjoni2117@gmail.com>
2025-10-25 00:17:48 +0800

    Create mlbb.js

---
+++ data/config/mlbb.js
@@ -0,0 +1,16 @@
+export const MLBB_CONFIGS = [
+  {
+    id: 1,
+    title: "Config No Lag Ultra",
+    image: "/img/config/mlbb_nolag.jpg",
+    description: "Mengurangi drop FPS, cocok untuk perangkat spek rendah/menengah.",
+    download: "#" // ganti nanti dengan link google drive/ mediafire
+  },
+  {
+    id: 2,
+    title: "Config Rank Booster V3",
+    image: "/img/config/mlbb_rank.jpg",
+    description: "Membantu stabilkan performa hero saat push rank.",
+    download: "#"
+  }
+];
