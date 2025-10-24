e8faf06d5ad574ac1417359223969f96129df92e
jonipes <bangjoni2117@gmail.com>
2025-10-25 00:01:11 +0800

    Create games.js

---
+++ data/games.js
@@ -0,0 +1,28 @@
+const GAMES = [
+  {
+    id: "efootball",
+    name: "eFootball",
+    tagline: "Top Up eFootball Coins",
+    image: "/img/efootball.jpg"
+  },
+  {
+    id: "mlbb",
+    name: "Mobile Legends",
+    tagline: "Diamond & Starlight",
+    image: "/img/mlbb.jpg"
+  },
+  {
+    id: "pubg",
+    name: "PUBG Mobile",
+    tagline: "UC Instan & Aman",
+    image: "/img/pubg.jpg"
+  },
+  {
+    id: "ff",
+    name: "Free Fire",
+    tagline: "Diamond Murah Cepat",
+    image: "/img/ff.jpg"
+  }
+];
+
+export default GAMES;
