f6b7662f0f7abe64b0eb0c22288542fdd62d7581
jonipes <bangjoni2117@gmail.com>
2025-10-25 09:54:41 +0800

    Update games.js

--- data/games.js
+++ data/games.js
@@ -1,28 +1,7 @@
 const GAMES = [
-  {
-    id: "efootball",
-    name: "eFootball",
-    tagline: "Top Up eFootball Coins",
-    image: "/img/efootball.jpg"
-  },
-  {
-    id: "mlbb",
-    name: "Mobile Legends",
-    tagline: "Diamond & Starlight",
-    image: "/img/mlbb.jpg"
-  },
-  {
-    id: "pubg",
-    name: "PUBG Mobile",
-    tagline: "UC Instan & Aman",
-    image: "/img/pubg.jpg"
-  },
-  {
-    id: "ff",
-    name: "Free Fire",
-    tagline: "Diamond Murah Cepat",
-    image: "/img/ff.jpg"
-  }
+  { id: "mlbb",      name: "Mobile Legends", tagline: "Diamonds & Starlight",  image: "/img/mlbb.jpg" },
+  { id: "pubg",      name: "PUBG Mobile",    tagline: "UC Instan & Aman",      image: "/img/pubg.jpg" },
+  { id: "efootball", name: "eFootball",      tagline: "Coins eFootball",       image: "/img/efootball.jpg", notReady: true },
+  { id: "ff",        name: "Free Fire",      tagline: "Diamond Free Fire",     image: "/img/ff.jpg",         notReady: true }
 ];
-
 export default GAMES;
